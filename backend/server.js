52d65a02e977286e064679370e9c53fd6f92b12c
saran <saransathi1010@gmail.com>
2024-12-26 16:08:29 +0530

    updating

---
+++ backend/server.js
@@ -0,0 +1,120 @@
+// server.js
+const WebSocket = require('ws');
+const server = new WebSocket.Server({ port: 8080 });
+
+// Store room and client information
+const rooms = new Map(); // roomId -> { clients: [], videoStates: Map }
+
+function broadcastToRoom(roomId, message, excludeClient = null) {
+  if (rooms.has(roomId)) {
+    const room = rooms.get(roomId);
+    room.clients.forEach(client => {
+      if (client !== excludeClient && client.ws.readyState === WebSocket.OPEN) {
+        client.ws.send(JSON.stringify(message));
+      }
+    });
+  }
+}
+
+function updateRoomStatus(roomId) {
+  const room = rooms.get(roomId);
+  if (room) {
+    const roomStatus = {
+      type: 'roomStatus',
+      clients: room.clients.map(client => ({
+        id: client.id,
+        username: client.username,
+        hasFile: client.hasFile,
+        fileName: client.fileName
+      })),
+      totalMembers: room.clients.length
+    };
+    broadcastToRoom(roomId, roomStatus);
+  }
+}
+
+server.on('connection', (ws) => {
+  console.log('New client connected');
+  let clientInfo = {
+    roomId: null,
+    id: Math.random().toString(36).substr(2, 9),
+    ws: ws,
+    hasFile: false,
+    fileName: null,
+    username: null
+  };
+
+  ws.on('message', (message) => {
+    const data = JSON.parse(message);
+    console.log('Received message:', data.type);
+
+    switch (data.type) {
+      case 'joinRoom':
+        const roomId = data.roomId;
+        const username = data.username;
+        clientInfo.roomId = roomId;
+        clientInfo.username = username;
+
+        if (!rooms.has(roomId)) {
+          rooms.set(roomId, {
+            clients: [],
+            videoStates: new Map()
+          });
+        }
+
+        rooms.get(roomId).clients.push(clientInfo);
+        updateRoomStatus(roomId);
+
+        // Send current video state to new client
+        const room = rooms.get(roomId);
+        if (room.videoStates.size > 0) {
+          const currentState = Array.from(room.videoStates.values())[0];
+          ws.send(JSON.stringify({
+            type: 'syncState',
+            ...currentState
+          }));
+        }
+        break;
+
+      case 'fileSelected':
+        if (clientInfo.roomId) {
+          clientInfo.hasFile = true;
+          clientInfo.fileName = data.fileName;
+          updateRoomStatus(clientInfo.roomId);
+        }
+        break;
+
+      case 'play':
+      case 'pause':
+      case 'seek':
+      case 'timeUpdate':
+        if (clientInfo.roomId) {
+          const room = rooms.get(clientInfo.roomId);
+          room.videoStates.set(clientInfo.id, {
+            isPlaying: data.isPlaying,
+            currentTime: data.currentTime,
+            duration: data.duration
+          });
+          broadcastToRoom(clientInfo.roomId, data, ws);
+        }
+        break;
+    }
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+    if (clientInfo.roomId && rooms.has(clientInfo.roomId)) {
+      const room = rooms.get(clientInfo.roomId);
+      room.clients = room.clients.filter(client => client.id !== clientInfo.id);
+      room.videoStates.delete(clientInfo.id);
+      
+      if (room.clients.length === 0) {
+        rooms.delete(clientInfo.roomId);
+      } else {
+        updateRoomStatus(clientInfo.roomId);
+      }
+    }
+  });
+});
+
+console.log('WebSocket server running on port 8080');
--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import VideoPlayer from "./components/VideoPlayer";
+import VideoPlayer1 from "./components/VideoPlayer1";
 
 function App() {
   return (
     <div>
       <h1>Sync Video Player</h1>
-      <VideoPlayer />
+      {/* <VideoPlayer /> */}
+      <VideoPlayer1 />
     </div>
   );
 }
